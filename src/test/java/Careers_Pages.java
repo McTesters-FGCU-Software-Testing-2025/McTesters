29f426b50d769f21fab37eaa8d753a59417882ad
Tanner <tjlang1120@gmail.com>
2025-04-03 17:35:29 -0400

    Finished 2 unit tests with 10 total test cases

--- src/test/java/Careers_Pages.java
+++ src/test/java/Careers_Pages.java
@@ -1,4 +1,6 @@
 import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -9,7 +11,9 @@ import org.testng.annotations.AfterClass;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 //8 classes, 5 methods/test per class.
 // Running the testng.xml to test our methods
@@ -29,7 +33,17 @@ public class Careers_Pages {
     public void beforeMethod() {
         System.out.println("Initializing Method: [" + ( ++count_vars.method_Count ) + "]" );
         System.setProperty("webdriver.firefox.driver", "C://Program Files/Java/firefox-driver/geckodriver.exe");
-        driver = new FirefoxDriver();
+
+
+        //To Adjust Firefox options.
+        FirefoxOptions options = new FirefoxOptions();
+        FirefoxProfile profile = new FirefoxProfile();
+
+        // disabling firefox "Use your location" prompt
+        profile.setPreference("permissions.default.geo", 2); //2 denies permission
+
+        options.setProfile(profile);
+        driver = new FirefoxDriver(options); //now init the driver with my options
     }
 
     //System.out.println("Method [" + (++method_Count) + "]: Complete");
@@ -59,20 +73,22 @@ public class Careers_Pages {
         WebElement careerLink = driver.findElement(By.linkText("Careers"));
         careerLink.click();
         JavascriptExecutor js = (JavascriptExecutor) driver;
+        Thread.sleep(500);
 
-        //scroll to bottom
-        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-        Thread.sleep(3000);
-
-        //scroll back up to bring the jobs into view.
-        WebElement jobs  = driver.findElement(By.className("l-layout__content-text"));
-        js.executeScript("arguments[0].scrollIntoView(true);", jobs);
-
-        Thread.sleep(3000);
-
+        js.executeScript(
+                "let totalHeight = 0;" +
+                        "let distance = 100;" +
+                        "let timer = setInterval(() => {" +
+                        "  window.scrollBy(0, distance);" +
+                        "  totalHeight += distance;" +
+                        "  if (totalHeight >= document.body.scrollHeight) clearInterval(timer);" +
+                        "}, 100);"
+        );
+        Thread.sleep(2000);
+        System.out.println("Test Complete");
     }
 
-    @Test(priority = 2)
+    @Test(priority = 2, groups = "Careers Pages")
     public void restaurant_jobs() throws InterruptedException
     {
         getURL("https://careers.mcdonalds.com/");
@@ -80,54 +96,133 @@ public class Careers_Pages {
 
         WebElement jobs = driver.findElement(By.className("l-layout__content-text"));
         js.executeScript("arguments[0].scrollIntoView(true);", jobs);
-        Thread.sleep(3000);
+        Thread.sleep(1500);
 
         // Find and click on restaurant jobs.
         WebElement restaurantJobs = driver.findElement(By.linkText("Restaurant Jobs"));
         restaurantJobs.click();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
 
-        // Use Actions class to simulate real user scrolling
+        // creating an action to use to scroll.
         Actions actions = new Actions(driver);
 
-        // Scroll down using repeated PAGE_DOWN keypresses
+        // i have Scroll down using repeated PAGE_DOWN keypresses since this page is built on react, and
+        //i have been having trouble with the scroll command.
         for (int i = 0; i < 20; i++) {
             actions.sendKeys(Keys.PAGE_DOWN).perform();
             Thread.sleep(300); // adjust timing for smoother scroll
         }
 
-        Thread.sleep(3000); // optional: pause to visually confirm you're at bottom
+        Thread.sleep(2000); // optional: pause to visually confirm you're at bottom
+        System.out.println("Test Complete");
     }
 
 
-    @Test(priority = 3)
+    @Test(priority = 3, groups = "Careers Pages")
     public void corporate_careers() throws InterruptedException {
 
-        System.out.println("Hello World_3");
-        Thread.sleep(3000);
+        getURL("https://careers.mcdonalds.com/");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        WebElement jobs = driver.findElement(By.className("l-layout__content-text"));
+        js.executeScript("arguments[0].scrollIntoView(true);", jobs);
+        Thread.sleep(1500);
+
+        // Find and click on corporate jobs.
+        WebElement restaurantJobs = driver.findElement(By.linkText("Corporate Careers"));
+        restaurantJobs.click();
+        Thread.sleep(1500);
 
 
+        //scroll to bottom of new page.
+        js.executeScript(
+                "let totalHeight = 0;" +
+                        "let distance = 150;" +
+                        "let timer = setInterval(() => {" +
+                        "  window.scrollBy(0, distance);" +
+                        "  totalHeight += distance;" +
+                        "  if (totalHeight >= document.body.scrollHeight) clearInterval(timer);" +
+                        "}, 100);"
+        );
+        Thread.sleep(5000);
+        System.out.println("Test Complete");
 
     }
 
-    @Test(priority = 4)
+    @Test(priority = 4, groups = "Careers Pages")
     public void become_a_franchisee() throws InterruptedException {
 
-        System.out.println("Hello World_4");
-        Thread.sleep(3000);
+        getURL("https://careers.mcdonalds.com/");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        js.executeScript("scrollTo(0,500)");
+        Thread.sleep(1500);
+
+        //save the original tab
+        String originalWindow = driver.getWindowHandle();
+
+        // Find and click on corporate jobs.
+        WebElement franchise = driver.findElement(By.linkText("Become a Franchisee"));
+        franchise.click();
+        Thread.sleep(1500);
 
 
+        // set of the tabs
+        Set<String> windowHandles = driver.getWindowHandles();
+        List<String> handlesList = new ArrayList<>(windowHandles);
+
+        //switch to last
+        driver.switchTo().window(handlesList.get(handlesList.size() - 1));
+
+
+        js = (JavascriptExecutor) driver;
+        //scroll to bottom of new page.
+        js.executeScript(
+                "let totalHeight = 0;" +
+                        "let distance = 250;" +
+                        "let timer = setInterval(() => {" +
+                        "  window.scrollBy(0, distance);" +
+                        "  totalHeight += distance;" +
+                        "  if (totalHeight >= document.body.scrollHeight) clearInterval(timer);" +
+                        "}, 100);"
+        );
+        Thread.sleep(3500);
+
+        driver.close(); //close the franchisee window
+
+        // switch back to main window
+        driver.switchTo().window(originalWindow);
+
+        Thread.sleep(500);
+        System.out.println("Test Complete");
+        //original window should now close on AfterMethod
     }
 
-    @Test(priority = 5)
+    @Test(priority = 5, groups = "Careers Pages")
     public void university_careers() throws InterruptedException {
 
-        System.out.println("Hello World_5");
-        Thread.sleep(3000);
+        getURL("https://careers.mcdonalds.com/");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
 
+        js.executeScript("scrollTo(0,500)");
+        Thread.sleep(1500);
 
+        // Find and click on corporate jobs.
+        WebElement restaurantJobs = driver.findElement(By.linkText("University"));
+        restaurantJobs.click();
+        Thread.sleep(1500);
+
+        js.executeScript(
+                "let totalHeight = 0;" +
+                        "let distance = 250;" +
+                        "let timer = setInterval(() => {" +
+                        "  window.scrollBy(0, distance);" +
+                        "  totalHeight += distance;" +
+                        "  if (totalHeight >= document.body.scrollHeight) clearInterval(timer);" +
+                        "}, 100);"
+        );
+        Thread.sleep(1500);
+        System.out.println("Test Complete");
     }
-
-
 }
 
--- src/test/java/Find_Your_Restaurant.java
+++ src/test/java/Find_Your_Restaurant.java
@@ -1,9 +1,14 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.*;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.WebDriver;
+
+import java.util.List;
 
 //8 classes, 5 methods/test per class.
 // Running the testng.xml to test our methods
@@ -11,7 +16,6 @@ import org.openqa.selenium.WebDriver;
 //TestNG hierarchy: Test Suite -> Test -> Class -> Method
 
 public class Find_Your_Restaurant {
-
 
     WebDriver driver;
 
@@ -21,13 +25,22 @@ public class Find_Your_Restaurant {
 
     //Occurs before every 'method' annotated with @Test, every single one
     @BeforeMethod
-    public void beforeMethod() {
+    public void beforeMethod()
+    {
         System.out.println("Initializing Method: [" + ( ++count_vars.method_Count ) + "]" );
         System.setProperty("webdriver.firefox.driver", "C://Program Files/Java/firefox-driver/geckodriver.exe");
-        driver = new FirefoxDriver();
+
+        //To Adjust Firefox options.
+        FirefoxOptions options = new FirefoxOptions();
+        FirefoxProfile profile = new FirefoxProfile();
+
+        // disabling firefox "Use your location" prompt
+        profile.setPreference("permissions.default.geo", 2); //2 denies permission
+
+        options.setProfile(profile);
+        driver = new FirefoxDriver(options); //now init the driver with my options
     }
 
-    //System.out.println("Method [" + (++method_Count) + "]: Complete");
 
     //Occurs after every 'method' annotated with @Test, every single one
     @AfterMethod
@@ -40,54 +53,169 @@ public class Find_Your_Restaurant {
     @AfterClass
     public void afterClass() { System.out.println("Class [" + ( count_vars.class_Count ) + "]: Complete"); }
 
-
-    @Test(priority = 1)
-    public void Test_1() throws InterruptedException {
-        System.out.println("Hello World_1");
-        driver.get("https://www.mcdonalds.com/us/en-us.html");
+    public void getURL(String url) throws InterruptedException
+    {
+        driver.get(url);
+        driver.manage().window().maximize();
         Thread.sleep(3000);
-
     }
 
+    @Test(priority = 1, groups = "Find Your Restaurant")
+    public void find_Your_Restaurant_Link() throws InterruptedException
+    {
+        getURL("https://www.mcdonalds.com/us/en-us.html");  //start at home page of donnies.
 
+        JavascriptExecutor js = (JavascriptExecutor) driver;    //init js executor for scrolling
 
-    @Test(priority = 2)
-    public void Test_2() throws InterruptedException {
-
-        System.out.println("Hello World_2");
+        //find and click on Find Your Restaurant link!
+        WebElement findYour = driver.findElement(By.linkText("Find Your Restaurant"));
+        findYour.click();
         Thread.sleep(3000);
 
+        //scroll down to bring the search field into view.
+        WebElement searchField = driver.findElement(By.name("search-val"));
+        js.executeScript("arguments[0].scrollIntoView(true);", searchField);
 
-
-    }
-
-    @Test(priority = 3)
-    public void Test_3() throws InterruptedException {
-
-        System.out.println("Hello World_3");
-        Thread.sleep(3000);
-
-
-
-    }
-
-    @Test(priority = 4)
-    public void Test_4() throws InterruptedException {
-
-        System.out.println("Hello World_4");
-        Thread.sleep(3000);
-
-
-    }
-
-    @Test(priority = 5)
-    public void Test_5() throws InterruptedException {
-
-        System.out.println("Hello World_5");
+        System.out.println("Test Complete");
         Thread.sleep(3000);
 
 
     }
 
 
+
+    @Test(priority = 2, groups = "Find Your Restaurant")
+    public void searchField_Test() throws InterruptedException
+    {
+
+        getURL("https://www.mcdonalds.com/us/en-us/restaurant-locator.html");  //start at 'find your' page
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;    //init js executor for scrolling
+        Thread.sleep(1500);
+
+        //scroll to search field:
+        //scroll down to bring the search field into view.
+        WebElement searchField = driver.findElement(By.id("form-text-1673594539"));
+        js.executeScript("arguments[0].scrollIntoView(true);", searchField);
+        Thread.sleep(1500);
+
+        //Enter Location
+        searchField.sendKeys("Florida Gulf Coast University");
+        Thread.sleep(1500);
+        searchField.sendKeys(Keys.ENTER); //search
+        Thread.sleep(1500);
+
+        //scroll down to bring the results into view.
+        js.executeScript("window.scrollBy(0,200);");
+        Thread.sleep(1500);
+
+        //select first result and click on it to test to see if it works.
+        WebElement firstResult = driver.findElement(By.linkText("17800 Ben Hill Griffin Pky"));
+        firstResult.click();
+        Thread.sleep(1000);
+
+        //SCROLL
+        js.executeScript(
+                "let totalHeight = 0;" +
+                        "let distance = 500;" +
+                        "let timer = setInterval(() => {" +
+                        "  window.scrollBy(0, distance);" +
+                        "  totalHeight += distance;" +
+                        "  if (totalHeight >= document.body.scrollHeight) clearInterval(timer);" +
+                        "}, 100);"
+        );
+        System.out.println("Test Complete");
+        Thread.sleep(2000);
+
+    }
+
+    @Test(priority = 3, groups = "Find Your Restaurant")
+    public void filterServices_CheckBoxes_Test() throws InterruptedException
+    {
+        getURL("https://www.mcdonalds.com/us/en-us/restaurant-locator.html");  //start at 'find your' page
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;    //init js executor for scrolling
+        Thread.sleep(1500);
+
+        //scroll down to bring the search field into view.
+        WebElement searchField = driver.findElement(By.id("form-text-1673594539"));
+        js.executeScript("arguments[0].scrollIntoView(true);", searchField);
+        Thread.sleep(1500);
+
+        //select filters dropdown
+        WebElement filters = driver.findElement(By.cssSelector("div.cmp-restaurant-locator__filter-btn > button"));
+        filters.click();
+        Thread.sleep(1500);
+
+        //loop through the checkboxes and click each one-by-one.
+        for (int i = 0; i < 10; i++)
+        {
+            String checkBox = "filterItemsInput" + i;   //simple concatenation
+            WebElement checkBoxes = driver.findElement(By.id(checkBox));
+            js.executeScript("arguments[0].click();", checkBoxes);
+            Thread.sleep(500);
+        }
+        System.out.println("Test Complete");
+    }
+
+    @Test(priority = 4, groups = "Find Your Restaurant")
+    public void filterDistance_RadioFilters_Test() throws InterruptedException {
+
+        getURL("https://www.mcdonalds.com/us/en-us/restaurant-locator.html");  //start at 'find your' page
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;    //init js executor for scrolling
+        Thread.sleep(1500);
+
+        //scroll down to bring the search field into view.
+        WebElement searchField = driver.findElement(By.id("form-text-1673594539"));
+        js.executeScript("arguments[0].scrollIntoView(true);", searchField);
+        Thread.sleep(1500);
+
+        //select filters dropdown
+        WebElement filters = driver.findElement(By.cssSelector("div.cmp-restaurant-locator__filter-btn > button"));
+        filters.click();
+        Thread.sleep(1500);
+
+        //loop through the RadioFilters and click each one-by-one.
+        String[] values = {"5", "10", "20", "50"};
+        for (String val : values)
+        {
+            WebElement radioFilters = driver.findElement
+                    (By.cssSelector("input[type='radio'][name='radio-filter'][value='" + val + "']"));
+            js.executeScript("arguments[0].click();", radioFilters);
+            Thread.sleep(500);
+        }
+        System.out.println("Test Complete");
+    }
+
+    @Test(priority = 5, groups = "Find Your Restaurant")
+    public void drag_Map_test() throws InterruptedException
+    {
+
+        getURL("https://www.mcdonalds.com/us/en-us/restaurant-locator.html");  //start at 'find your' page
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;    //init js executor for scrolling
+        Thread.sleep(1500);
+
+        //scroll down to bring google map into frame
+        //scroll down to bring the results into view.
+        js.executeScript("window.scrollBy(0,600);");
+        Thread.sleep(1500);
+
+
+        //grab map
+        WebElement map = driver.findElement(By.className("map"));
+
+        //create new action for dragging
+        Actions drag = new Actions(driver);
+        //perform click and drag action
+        drag.clickAndHold(map).moveByOffset(300,50).release().build().perform();
+        Thread.sleep(1500);
+        drag.clickAndHold(map).moveByOffset(-300,-50).release().build().perform();  //drag back
+        Thread.sleep(1500);
+
+        //wait
+        Thread.sleep(1500);
+        System.out.println("Test Complete");
+    }
 }
